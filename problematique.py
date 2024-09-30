65ea03e2d9cc27a31c1290de9b2a7041dc012380
DannickB <bild2707@usherbrooke.ca>
2024-09-30 19:29:31 -0400

    sounds better

--- problematique.py
+++ problematique.py
@@ -1,9 +1,10 @@
-import soundfile as sf
+import scipy.signal as ss
 import numpy as np
 import matplotlib.pyplot as plt
 import wave
 import struct
-from scipy.io.wavfile import write
+import scipy.io.wavfile as wav
+import sounddevice as sd
 
 def frequencyMap():
     freqs = \
@@ -25,10 +26,15 @@ def frequencyMap():
 def extract_params(X):
     amp = []
     ph = []
-    id_fund = np.argmax(abs(X))
-    for i in range(0, 33):
-        amp.append(abs(X[id_fund * i]))
-        ph.append(np.angle(X[id_fund * i]))
+    fund = np.argmax(abs(X))
+    peak_ids, _ = ss.find_peaks(np.abs(X[0:int(len(X)/2)]), distance=200)
+    peaks=[]
+    for peak in peak_ids:
+        peaks.append(X[peak])
+    peaks = sorted(peaks, reverse=True, key=abs)
+    for i in range(0, 32):
+        amp.append(abs(peaks[i]))
+        ph.append(np.angle(peaks[i]))
     return amp, ph
 
 
@@ -43,35 +49,26 @@ def find_order(freq, target):
     return N
 
 
-def synthesis_note(N, amps, ph, freq, env, fe):
+def synthesis_note(N, amps, ph, freq, env, fe, signal):
     t = np.linspace(0, (N/fe), N)
     x = []
     for dt in t:
         val = 0
-        for i in range(33):
-            val += amps[i] * np.sin(2*np.pi*freq*i*dt + ph[i])
+        for i in range(len(amps)):
+            val += amps[i] * np.sin(2*np.pi*freq*i*dt - ph[i])
         x.append(val)
-    return np.multiply(x, env)
-
-def save_audio(audio, sampleRate, filename):
-    out = wave.open(filename, 'wb')
-    out.setnchannels(1)
-    out.setframerate(sampleRate)
-    out.setsampwidth(2)
-    for frame in audio:
-        data = struct.pack('<h', int(frame*8))
-        out.writeframes(data)
-    out.close()
+    x = np.multiply(x, env)
+    x *= max(np.abs(signal) / max(np.abs(x)))
+    return x
 
 if __name__ == "__main__":
-    signal, fe = sf.read('note_guitare_lad.wav')
+    # signal, fe = sf.read('note_guitare_lad.wav')
+    fe, signal = wav.read('note_guitare_lad.wav')
     frequency_map = frequencyMap()
     f = frequency_map["LA#"]
     N = len(signal)
-    w = np.hamming(N)
-    X = np.fft.fft(signal, N)
-    Xw = np.multiply(X, w)
-    amp, ph = extract_params(Xw)
+    X = np.fft.fft(signal, fe)
+    amp, ph = extract_params(X[0:int(fe/2)])
 
     filter_freq = np.pi / 1000
     target_gain = np.sqrt(2) / 2
@@ -86,17 +83,17 @@ if __name__ == "__main__":
     env_max = env[np.argmax(env)]
     env = env/env_max
 
-
-    sol = synthesis_note(N, amp, ph, frequency_map["SOL"], env[0:N], fe)
-    mi = synthesis_note(N, amp, ph, frequency_map["RE#"], env[0:N], fe) #Re# et Mi bemole sont la meme
-    fa = synthesis_note(N, amp, ph, frequency_map["FA"], env[0:N], fe)
-    re = synthesis_note(N, amp, ph, frequency_map["RE"], env[0:N], fe)
+    #Creating desired notes
+    sol = synthesis_note(N, amp, ph, frequency_map["LA#"], env[0:N], fe, signal)
+    mi = synthesis_note(N, amp, ph, frequency_map["RE#"], env[0:N], fe, signal) #Re# et Mi bemole sont la meme
+    fa = synthesis_note(N, amp, ph, frequency_map["FA"], env[0:N], fe, signal)
+    re = synthesis_note(N, amp, ph, frequency_map["RE"], env[0:N], fe, signal)
     silence = np.zeros(int(fe/2))
 
+    #Stripping notes because the full duration is too long for the melody
     t_quarter = int(N/4)
     t_demi = int(N/2)
     symphony = np.concat((sol[0:t_quarter], sol[0:t_quarter], sol[0:t_quarter], mi[0:t_demi], silence, fa[0:t_quarter], fa[0:t_quarter], fa[0:t_quarter], re))
-
     a1 = plt.subplot(3, 1, 1)
     a1.set_title("Base sample")
     a1.plot(signal)
@@ -106,6 +103,7 @@ if __name__ == "__main__":
     a3 = plt.subplot(3, 1, 3)
     a3.set_title("envloppe")
     a3.plot(env)
-    plt.show()
-    save_audio(symphony.tolist(), fe, "symphony.wav")
+    # plt.show()
+    sd.play(np.int16(np.real(symphony)), fe)
+    wav.write("symphony.wav", fe, np.int16(np.real(symphony)))
 
